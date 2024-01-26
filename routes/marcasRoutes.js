42f1f75f1ef89a83d714869627fee1cadb8cddd1
ioskpu <lrcorales@gmail.com>
2024-01-25 20:24:22 -0500

    se crea la carpeta routes para empezar a crear las rutas, se crea la ruta de marcasRoutes.js

---
+++ routes/marcasRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const marcasController = require('../controladores/marcasController');
+
+//rutas CRUD para marcas de vehiculos
+
+//crear marca
+router.post('/marcas', marcasController.crearMarca);
+
+//obtener todas las marcas
+router.get('/marcas', marcasController.getAllMarcas);
+
+//obtener una marca por ID
+router.get('/marcas/:id', marcasController.getMarcaById);
+
+//actualizar una marca
+router.put('/marcas/:id', marcasController.actualizarMarca);
+
+//eliminar una marca
+router.delete('/marcas/:id', marcasController.deleteMarcaById);
+
+module.exports = router;
