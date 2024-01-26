b89e1411b0fbd37b72d4bbc97ed375e98b5fc480
ioskpu <lrcorales@gmail.com>
2024-01-26 07:29:03 -0500

    se reestructura todo el proyecto para mejor control y buenas pratcticas

--- cliente/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import create from 'zustand';
-
-const useMarcaStore = create((set) => ({
-  marcas: [],
-  setMarcas: (nuevasMarcas) => set({ marcas: nuevasMarcas }),
-}));
-
-export default useMarcaStore;
